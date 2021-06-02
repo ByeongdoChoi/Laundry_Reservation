da916295b8239f837f67528dc31523380a2ef96e
SeokNamLee <hhhhggggg@naver.com>
2021-06-02 12:32:09 +0900

    feat: Main

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "member_init";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //////회원 정보가 없으면 회원가입 창으로 감
+        if(user == null){
+            GotoActivity(SignUp.class);
+        }
+
+        else{
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference docRef = db.collection("users").document(user.getUid());
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if(document != null){
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d(TAG, "No such document");
+                                GotoActivity(member_init.class);
+                            }
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
+
+        findViewById(R.id.logout).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+      @Override
+      public void onClick(View v){
+          switch(v.getId()){
+              case R.id.logout:
+                  FirebaseAuth.getInstance().signOut();
+                  GotoActivity(SignUp.class);
+                  break;
+          }
+      }
+    };
+
+    private void GotoActivity(Class c){
+        Intent intent = new Intent(this,c);
+        startActivity(intent);
+    }
+}
